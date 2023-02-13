7f09e8f6ebfbd2b1c1b0433ae943754114ca18a5
Dae R. Jeong <threeearcat@gmail.com>
2023-02-13 04:29:25 +0000

    fbmem: suppress surplus  warning

--- drivers/video/fbdev/core/fbmem.c
+++ drivers/video/fbdev/core/fbmem.c
@@ -1022,7 +1022,7 @@ fb_set_var(struct fb_info *info, struct fb_var_screeninfo *var)
 	/* verify that virtual resolution >= physical resolution */
 	if (var->xres_virtual < var->xres ||
 	    var->yres_virtual < var->yres) {
-		pr_warn("WARNING: fbcon: Driver '%s' missed to adjust virtual screen size (%ux%u vs. %ux%u)\n",
+		pr_debug("WARNING: fbcon: Driver '%s' missed to adjust virtual screen size (%ux%u vs. %ux%u)\n",
 			info->fix.id,
 			var->xres_virtual, var->yres_virtual,
 			var->xres, var->yres);
